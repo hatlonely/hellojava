ce9ab51045b3642c9f7f16dcf41212a012168cb4
hatlonely <hatlonely@gmail.com>
2018-03-12 23:02:36 +0800

    update

---
+++ src/test/java/buildin/ArrayTest.java
@@ -0,0 +1,26 @@
+package buildin;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class ArrayTest {
+    @Test
+    public void testArray() {
+        int[] iarr1 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        assertEquals(iarr1[3], 3);
+        assertEquals(iarr1.length, 10);
+
+        int[] iarr2 = new int[5];
+        Arrays.fill(iarr2, 6);
+        assertArrayEquals(iarr2, new int[] { 6, 6, 6, 6, 6 });
+
+        int[] iarr3 = { 5, 8, 1, 2, 0, 1 };
+        Arrays.sort(iarr3);
+        assertArrayEquals(iarr3, new int[] { 0, 1, 1, 2, 5, 8 });
+
+        assertArrayEquals(Arrays.copyOf(iarr1, 5), new int[] { 0, 1, 2, 3, 4 });
+        assertArrayEquals(Arrays.copyOfRange(iarr1, 3, 8), new int[] { 3, 4, 5, 6, 7 });
+    }
+}
