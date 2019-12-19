9047972a503da7daf05d8b00e3fdcdd1f5480d1a
hatlonely <hatlonely@gmail.com>
2019-12-19 20:26:49 +0800

    update

--- src/test/java/javase/CollectionTest.java
+++ src/test/java/javase/CollectionTest.java
@@ -16,9 +16,20 @@ public class CollectionTest {
                 new HashSet<Integer>(),
                 new TreeSet<Integer>()
         )) {
+            // 新增
             for (int i = 0; i < 10; i++) {
                 c.add(i);
             }
+
+            // 遍历
+            for (Iterator<Integer> it = c.iterator(); it.hasNext(); ) {
+                System.out.println(it.next());
+            }
+            for (Integer i : c) {
+                System.out.println(i);
+            }
+
+            // 状态检查
             assertArrayEquals(c.toArray(), new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
             assertEquals(c.size(), 10);
             assertFalse(c.isEmpty());
@@ -26,26 +37,32 @@ public class CollectionTest {
                 assertTrue(c.contains(i));
             }
 
+            // 删除
             assertTrue(c.remove(4));
             assertFalse(c.remove(10));
             assertArrayEquals(c.toArray(), new Integer[]{0, 1, 2, 3, 5, 6, 7, 8, 9});
 
+            // 集合新增
             c.addAll(Arrays.asList(10, 11, 12));
             assertArrayEquals(c.toArray(), new Integer[]{0, 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12});
 
             assertTrue(c.containsAll(Arrays.asList(1, 3, 5, 7, 9)));
             assertFalse(c.containsAll(Arrays.asList(13, 14)));
 
+            // 集合删除
             assertTrue(c.removeAll(Arrays.asList(0, 10, 12)));
             assertArrayEquals(c.toArray(), new Integer[]{1, 2, 3, 5, 6, 7, 8, 9, 11});
 
+            // 条件删除
             assertTrue(c.removeIf((i) -> i % 2 == 0));
             assertArrayEquals(c.toArray(), new Integer[]{1, 3, 5, 7, 9, 11});
             assertFalse(c.removeIf((i) -> i > 20));
 
+            // 交集
             assertTrue(c.retainAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9)));
             assertArrayEquals(c.toArray(), new Integer[]{1, 3, 5, 7, 9});
 
+            // 清空
             c.clear();
             assertTrue(c.isEmpty());
         }
