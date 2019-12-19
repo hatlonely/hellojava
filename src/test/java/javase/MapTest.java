2bc88fba8fc475db3e8058d74172bc950d688daf
hatlonely <hatlonely@gmail.com>
2019-12-20 02:59:56 +0800

    update

--- src/test/java/javase/MapTest.java
+++ src/test/java/javase/MapTest.java
@@ -3,46 +3,73 @@ package javase;
 import org.junit.Test;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static org.junit.Assert.*;
 
 public class MapTest {
     @Test
-    public void testMap2() {
+    public void testMap() {
         for (final Map<String, String> m : Arrays.asList(
                 new TreeMap<String, String>(),
                 new HashMap<String, String>(),
-                new Hashtable<String, String>()
+                new Hashtable<String, String>(),
+                new ConcurrentHashMap<String, String>()
         )) {
+            assertTrue(m.isEmpty());
+            for (int i = 0; i < 5; i++) {
+                m.put("key" + i, "val" + i);
+            }
+            System.out.println(m);
 
+            for (int i = 0; i < 5; i++) {
+                assertTrue(m.containsKey("key" + i));
+                assertEquals(m.get("key" + i), "val" + i);
+            }
+            assertEquals(m.size(), 5);
+            assertFalse(m.isEmpty());
+            assertFalse(m.containsKey("key6"));
+
+            assertEquals(m.remove("key3"), "val3");
+            assertEquals(m.remove("key3"), null);
+
+            m.putAll(Map.of(
+                    "key2", "val2",
+                    "key3", "val3",
+                    "key4", "val4"
+            ));
+            System.out.println(m);
+            final Set keys = m.keySet();
+            for (int i = 0; i < 5; i++) {
+                assertTrue(keys.contains("key" + i));
+            }
+            final Collection vals = m.values();
+            for (int i = 0; i < 5; i++) {
+                assertTrue(vals.contains("val" + i));
+            }
+
+            final Set<Map.Entry<String, String>> entries = m.entrySet();
+            for (final Map.Entry it : entries) {
+                System.out.println(it.getKey() + "=>" + it.getValue());
+            }
+            m.forEach((k, v) -> System.out.println(k + " => " + v));
+
+            assertEquals(m.getOrDefault("key3", "val33"), "val3");
+            assertEquals(m.getOrDefault("key9", "val99"), "val99");
+            assertEquals(m.putIfAbsent("key3", "val33"), "val3");   // 无值才插入
+            assertEquals(m.replace("key3", "val33"), "val3");       // 有值才替换
+            assertEquals(m.replace("key9", "val99"), null);
+            assertEquals(m.get("key9"), null);
+            assertEquals(m.get("key3"), "val33");
+            assertFalse(m.replace("key3", "val3", "val33"));    // 老值相等才修改
+            assertTrue(m.replace("key3", "val33", "val3"));
+
+            assertEquals(m.compute("key3", (k, v) -> (k + v)), "key3val3");
+            assertEquals(m.get("key3"), "key3val3");
+            assertEquals(m.computeIfPresent("key5", (k, v) -> (k + v)), null);
+            assertEquals(m.computeIfAbsent("key5", (k) -> (k.replace("key", "val"))), "val5");
+
+            assertEquals(m.merge("key5", "nv", (ov, nv) -> (ov + nv)), "val5nv");
         }
-    }
-
-    @Test
-    public void testMap() {
-        final Map<String, String> hmss = new HashMap<>();
-        hmss.put("key1", "val1");
-        hmss.put("key2", "val2");
-        hmss.put("key3", "val3");
-
-        assertFalse(hmss.isEmpty());
-        assertTrue(hmss.containsKey("key1"));
-        assertEquals(hmss.size(), 3);
-        assertEquals(hmss.get("key2"), "val2");
-
-        System.out.println(hmss.keySet());
-        System.out.println(hmss.values());
-
-        for (final Map.Entry<String, String> entry : hmss.entrySet()) {
-            System.out.println(entry.getKey());
-            System.out.println(entry.getValue());
-        }
-
-        hmss.forEach((k, v) -> System.out.println(k + " => " + v));
-        hmss.remove("key2");
-        assertEquals(hmss.get("key2"), null);
-
-        final Map<String, String> tmss = new TreeMap<>();
-        assertEquals(tmss.getOrDefault("key6", "val6"), "val6");
     }
 }
