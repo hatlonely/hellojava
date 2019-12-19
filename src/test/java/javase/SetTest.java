29293683b64fda49f02927426de018a04041368a
hatlonely <hatlonely@gmail.com>
2019-12-20 00:13:42 +0800

    update

--- src/test/java/javase/SetTest.java
+++ src/test/java/javase/SetTest.java
@@ -1,40 +1,37 @@
 package javase;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import java.util.*;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeSet;
-
-import org.junit.Test;
 
 public class SetTest {
     @Test
     public void testSet() {
-        Set<Integer> hsi = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 5, 6, 6, 4, 3));
-        System.out.println(hsi);
-
-        assertFalse(hsi.isEmpty());
-        assertTrue(hsi.contains(5));
-        assertEquals(hsi.size(), 6);
-
-        hsi.removeIf(i -> (i % 2 == 1));
-        hsi.stream().forEach(System.out::println);
-
-        hsi.add(8);
-        hsi.add(0);
-
-        hsi.stream().map(i -> (i + 100)).forEach(System.out::println);
-
-        for (int i : hsi) {
-            System.out.println(i);
+        // Set 没有特有的方法，所有的方法继承自 Collection
+        for (final Set<String> s : Arrays.asList(
+                new HashSet<String>(),
+                new TreeSet<String>(),
+                new LinkedHashSet<String>()
+        )) {
+            for (int i = 0; i < 10; i++) {
+                assertFalse(s.contains("test" + i));
+            }
+            for (int i = 0; i < 10; i++) {
+                s.add("test" + i);
+            }
+            for (int i = 0; i < 10; i++) {
+                assertTrue(s.contains("test" + i));
+            }
+            System.out.println(s);
+            for (int i = 0; i < 10; i++) {
+                s.remove("test" + i);
+            }
+            for (int i = 0; i < 10; i++) {
+                assertFalse(s.contains("test" + i));
+            }
         }
-
-        Set<Integer> tsi = new TreeSet<>(Arrays.asList(1, 2, 3, 4, 5));
-        tsi.forEach((i) -> System.out.println(i));
-        tsi.stream().filter((i) -> (i % 2 == 1)).map((i) -> (i + 10)).forEach(System.out::println);
     }
 }
