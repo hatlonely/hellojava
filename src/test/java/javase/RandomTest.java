96bd1c1d60cb0b88faba50183ea5a84fa5fe0711
hatlonely <hatlonely@gmail.com>
2019-12-20 23:05:04 +0800

    update

---
+++ src/test/java/javase/RandomTest.java
@@ -0,0 +1,27 @@
+package javase;
+
+import org.junit.Test;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomTest {
+    public static void testThreadLocalRandom() {
+        System.out.println(ThreadLocalRandom.current().nextInt());
+        System.out.println(ThreadLocalRandom.current().nextLong());
+        System.out.println(ThreadLocalRandom.current().nextFloat());
+        System.out.println(ThreadLocalRandom.current().nextDouble());
+    }
+
+    @Test
+    public void testRandom() {
+        // java.util.Random的java.util.Random是线程安全的。
+        // 但是，跨线程的同时使用java.util.Random实例可能会遇到争用，从而导致性能下降。
+        // 在多线程设计中考虑使用ThreadLocalRandom
+        final Random r = new Random();
+        System.out.println(r.nextInt());
+        System.out.println(r.nextLong());
+        System.out.println(r.nextFloat());
+        System.out.println(r.nextDouble());
+    }
+}
