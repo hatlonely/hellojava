ebcc147180fa6087bc9937d33c705324a829b6d2
hatlonely <hatlonely@gmail.com>
2019-12-26 14:58:53 +0800

    update

---
+++ src/test/java/javase/NumberTest.java
@@ -0,0 +1,26 @@
+package javase;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class NumberTest {
+    @Test
+    public void testInteger() {
+        double delta = 0.000001;
+
+        for (Number n : Arrays.asList(
+                (short) 10, 10, (long) 10, (float) 10.0, 10.0
+        )) {
+            assertEquals(n.shortValue(), 10);
+            assertEquals(n.intValue(), 10);
+            assertEquals(n.longValue(), 10);
+            assertEquals(n.byteValue(), (byte) 10);
+            assertEquals(n.floatValue(), 10.0, delta);
+            assertEquals(n.doubleValue(), 10.0, delta);
+            System.out.println(n.toString());
+        }
+    }
+}
