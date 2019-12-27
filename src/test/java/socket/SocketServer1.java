b9032c2d0864cee746433c1dc81288e9718b6830
hatlonely <hatlonely@gmail.com>
2019-12-27 16:28:22 +0800

    update

--- src/test/java/socket/SocketServer1.java
+++ src/test/java/socket/SocketServer1.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class SocketServer {
+public class SocketServer1 {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(6789);
 
