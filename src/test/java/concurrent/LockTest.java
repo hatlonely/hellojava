726afb445331f2c1cf002a2d144e47ea098d08d4
hatlonely <hatlonely@gmail.com>
2019-12-28 20:45:30 +0800

    update

--- src/test/java/concurrent/LockTest.java
+++ src/test/java/concurrent/LockTest.java
@@ -10,6 +10,7 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.StampedLock;
 
 public class LockTest {
     @Test
@@ -78,6 +79,52 @@ public class LockTest {
                     rwlock.readLock().lock();
                     set.contains(Math.abs(ThreadLocalRandom.current().nextInt() % 100));
                     rwlock.readLock().unlock();
+                }
+            });
+        }
+
+        try {
+            es.shutdown();
+            while (!es.awaitTermination(1, TimeUnit.SECONDS)) {
+                // nothing to do
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testStampedLock() {
+        class MyClass {
+            private int v = 0;
+        }
+
+        // 乐观锁
+        StampedLock lock = new StampedLock();
+        ExecutorService es = Executors.newCachedThreadPool();
+
+        long endTime = System.currentTimeMillis() + 100;
+        MyClass obj = new MyClass();
+        for (int i = 0; i < 4; i++) {
+            es.execute(() -> {
+                while (System.currentTimeMillis() < endTime) {
+                    long stamp = lock.writeLock();
+                    obj.v = Math.abs(ThreadLocalRandom.current().nextInt() % 100);
+                    lock.unlockWrite(stamp);
+                }
+            });
+
+            es.execute(() -> {
+                while (System.currentTimeMillis() < endTime) {
+                    int v;
+                    while (true) {
+                        long stamp = lock.tryOptimisticRead();  // 乐观读，不锁写
+                        v = obj.v;
+                        if (lock.validate(stamp)) {             // 如果读期间，有写入，这个判断会失败
+                            break;
+                        }
+                    }
+                    System.out.println(v);
                 }
             });
         }
