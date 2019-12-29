9fbd4b93ca12174c5b66215cd588350b2fd0a3bb
hatlonely <hatlonely@gmail.com>
2019-12-29 17:52:58 +0800

    update

--- src/test/java/util/ArrayTest.java
+++ src/test/java/util/ArrayTest.java
@@ -10,22 +10,33 @@ import static org.junit.Assert.assertEquals;
 public class ArrayTest {
     @Test
     public void testArray() {
-        int[] iarr1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        assertEquals(iarr1[3], 3);
-        assertEquals(iarr1.length, 10);
+        {
+            int[] ia = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+            assertEquals(ia[3], 3);
+            assertEquals(ia.length, 10);
+        }
 
-        // 填充
-        int[] iarr2 = new int[5];
-        Arrays.fill(iarr2, 6);
-        assertArrayEquals(iarr2, new int[]{6, 6, 6, 6, 6});
+        {
+            int[] ia = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+            assertEquals(Arrays.binarySearch(ia, 5), 5);
+        }
 
-        // 排序
-        int[] iarr3 = {5, 8, 1, 2, 0, 1};
-        Arrays.sort(iarr3);
-        assertArrayEquals(iarr3, new int[]{0, 1, 1, 2, 5, 8});
+        {
+            int[] ia = new int[5];
+            Arrays.fill(ia, 6);
+            assertArrayEquals(ia, new int[]{6, 6, 6, 6, 6});
+        }
 
-        // 切片
-        assertArrayEquals(Arrays.copyOf(iarr1, 5), new int[]{0, 1, 2, 3, 4});
-        assertArrayEquals(Arrays.copyOfRange(iarr1, 3, 8), new int[]{3, 4, 5, 6, 7});
+        {
+            int[] ia = {5, 8, 1, 2, 0, 1};
+            Arrays.sort(ia);
+            assertArrayEquals(ia, new int[]{0, 1, 1, 2, 5, 8});
+        }
+
+        {
+            int[] ia = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+            assertArrayEquals(Arrays.copyOf(ia, 5), new int[]{0, 1, 2, 3, 4});
+            assertArrayEquals(Arrays.copyOfRange(ia, 3, 8), new int[]{3, 4, 5, 6, 7});
+        }
     }
 }
