7fd2e96ddf6c55bebc636e024ca036837eb58a17
hatlonely <hatlonely@gmail.com>
2019-12-31 04:13:29 +0800

    update

--- src/test/java/util/NavigableSetTest.java
+++ src/test/java/util/NavigableSetTest.java
@@ -2,52 +2,45 @@ package util;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.NavigableSet;
-import java.util.TreeSet;
-
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-
 public class NavigableSetTest {
     @Test
     public void testNavigableSet() {
-        for (NavigableSet<String> s : Arrays.asList(
-                new TreeSet<String>()
-        )) {
-            for (int i = 0; i < 10; i++) {
-                s.add("test" + i);
-            }
-            assertEquals(s.lower("test6"), "test5"); // <
-            assertEquals(s.higher("test6"), "test7"); // >
-            assertEquals(s.floor("test6"), "test6"); // <=
-            assertEquals(s.ceiling("test6"), "test6"); // >=
-
-            s.add("test65");
-            s.add("test67");
-            assertEquals(s.floor("test66"), "test65"); // <=
-            assertEquals(s.ceiling("test66"), "test67"); // >=
-            s.remove("test65");
-            s.remove("test67");
-
-            assertEquals(s.pollFirst(), "test0");
-            assertEquals(s.pollLast(), "test9");
-
-            NavigableSet ds = s.descendingSet();
-            assertArrayEquals(ds.toArray(), new String[]{
-                    "test8", "test7", "test6", "test5", "test4", "test3", "test2", "test1"
-            });
-
-            NavigableSet hs1 = s.headSet("test3", true);
-            NavigableSet hs2 = s.headSet("test3", false);
-            NavigableSet ts1 = s.tailSet("test7", true);
-            NavigableSet ts2 = s.tailSet("test7", false);
-            NavigableSet ss = s.subSet("test3", true, "test7", false);
-            assertArrayEquals(hs1.toArray(), new String[]{"test1", "test2", "test3"});
-            assertArrayEquals(hs2.toArray(), new String[]{"test1", "test2"});
-            assertArrayEquals(ts1.toArray(), new String[]{"test7", "test8"});
-            assertArrayEquals(ts2.toArray(), new String[]{"test8"});
-            assertArrayEquals(ss.toArray(), new String[]{"test3", "test4", "test5", "test6"});
-        }
+//        for (NavigableSet<String> s : Arrays.asList(
+//                new TreeSet<String>()
+//        )) {
+//            for (int i = 0; i < 10; i++) {
+//                s.add("test" + i);
+//            }
+//            assertEquals(s.lower("test6"), "test5"); // <
+//            assertEquals(s.higher("test6"), "test7"); // >
+//            assertEquals(s.floor("test6"), "test6"); // <=
+//            assertEquals(s.ceiling("test6"), "test6"); // >=
+//
+//            s.add("test65");
+//            s.add("test67");
+//            assertEquals(s.floor("test66"), "test65"); // <=
+//            assertEquals(s.ceiling("test66"), "test67"); // >=
+//            s.remove("test65");
+//            s.remove("test67");
+//
+//            assertEquals(s.pollFirst(), "test0");
+//            assertEquals(s.pollLast(), "test9");
+//
+//            NavigableSet ds = s.descendingSet();
+//            assertArrayEquals(ds.toArray(), new String[]{
+//                    "test8", "test7", "test6", "test5", "test4", "test3", "test2", "test1"
+//            });
+//
+//            NavigableSet hs1 = s.headSet("test3", true);
+//            NavigableSet hs2 = s.headSet("test3", false);
+//            NavigableSet ts1 = s.tailSet("test7", true);
+//            NavigableSet ts2 = s.tailSet("test7", false);
+//            NavigableSet ss = s.subSet("test3", true, "test7", false);
+//            assertArrayEquals(hs1.toArray(), new String[]{"test1", "test2", "test3"});
+//            assertArrayEquals(hs2.toArray(), new String[]{"test1", "test2"});
+//            assertArrayEquals(ts1.toArray(), new String[]{"test7", "test8"});
+//            assertArrayEquals(ts2.toArray(), new String[]{"test8"});
+//            assertArrayEquals(ss.toArray(), new String[]{"test3", "test4", "test5", "test6"});
+//        }
     }
 }
--- src/test/java/util/SetTest.java
+++ src/test/java/util/SetTest.java
@@ -2,36 +2,58 @@ package util;
 
 import org.junit.Test;
 
-import java.util.*;
+import java.util.NavigableSet;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class SetTest {
     @Test
     public void testSet() {
         // Set 没有特有的方法，所有的方法继承自 Collection
-        for (Set<String> s : Arrays.asList(
-                new HashSet<String>(),
-                new TreeSet<String>(),
-                new LinkedHashSet<String>()
-        )) {
-            for (int i = 0; i < 10; i++) {
-                assertFalse(s.contains("test" + i));
-            }
-            for (int i = 0; i < 10; i++) {
-                s.add("test" + i);
-            }
-            for (int i = 0; i < 10; i++) {
-                assertTrue(s.contains("test" + i));
-            }
-            System.out.println(s);
-            for (int i = 0; i < 10; i++) {
-                s.remove("test" + i);
-            }
-            for (int i = 0; i < 10; i++) {
-                assertFalse(s.contains("test" + i));
-            }
+    }
+
+    @Test
+    public void testSortedSet() {
+        SortedSet<String> set = IntStream.range(0, 10).boxed().map(x -> "key" + x).collect(Collectors.toCollection(TreeSet::new));
+        assertEquals(set.first(), "key0");
+        assertEquals(set.last(), "key9");
+        assertThat(set.headSet("key3"), equalTo(Set.of("key0", "key1", "key2")));
+        assertThat(set.tailSet("key7"), equalTo(Set.of("key7", "key8", "key9")));
+        assertThat(set.subSet("key3", "key7"), equalTo(Set.of("key3", "key4", "key5", "key6")));
+    }
+
+    @Test
+    public void testNavigableSet() {
+        {
+            NavigableSet<String> set = IntStream.range(0, 10).boxed().map(x -> "key" + x).collect(Collectors.toCollection(TreeSet::new));
+            assertEquals(set.lower("key6"), "key5");    // <
+            assertEquals(set.higher("key6"), "key7");   // >
+            assertEquals(set.floor("key6"), "key6");    // <=
+            assertEquals(set.ceiling("key6"), "key6");  // >=
+            set.remove("key6");
+            assertEquals(set.floor("key6"), "key5");
+            assertEquals(set.ceiling("key6"), "key7");
+        }
+        {
+            NavigableSet<String> set = IntStream.range(0, 5).boxed().map(x -> "key" + x).collect(Collectors.toCollection(TreeSet::new));
+            assertEquals(set.pollFirst(), "key0");
+            assertThat(set, equalTo(Set.of("key1", "key2", "key3", "key4")));
+            assertEquals(set.pollLast(), "key4");
+            assertThat(set, equalTo(Set.of("key1", "key2", "key3")));
+        }
+        {
+            NavigableSet<String> set = IntStream.range(0, 10).boxed().map(x -> "key" + x).collect(Collectors.toCollection(TreeSet::new));
+            assertThat(set.descendingSet(), equalTo(Set.of("key9", "key8", "key7", "key6", "key5", "key4", "key3", "key2", "key1", "key0")));
+            assertThat(set.headSet("key3", false), equalTo(Set.of("key0", "key1", "key2")));
+            assertThat(set.tailSet("key7", true), equalTo(Set.of("key7", "key8", "key9")));
+            assertThat(set.subSet("key3", true, "key7", false), equalTo(Set.of("key3", "key4", "key5", "key6")));
         }
     }
 }
